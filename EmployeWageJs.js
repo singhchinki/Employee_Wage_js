0ba47841ee8b1f69308783b7272395b2dc85b946
chinki kumari singh <singhchinkikumari.com>
2022-07-04 18:19:04 +0530

    [Chinki] UC1 employee attendance

---
+++ EmployeWageJs.js
@@ -0,0 +1,14 @@
+{
+    const IS_ABSENT = 0
+    let empCheck = Math.floor(Math.random() * 10) % 2;
+    if (empCheck == IS_ABSENT) {
+        console.log("Uc1 - Employee is absent");
+        return;
+        
+    }
+    else{
+        console.log("Uc1- Employee is Present");
+        
+    }
+
+}
---
+++ add.js
@@ -0,0 +1,15 @@
+let a = 10;
+let b = 20;
+let c = a + b;
+console.log("Addition is : "+c) ;
+
+let s1 = "10";
+let s2 = "20";
+let s3 = s1 + s2;
+console.log("str is : "+s3) ;
+
+
+let d = new Date(); //creates a varible which contain date
+let today = "Today is: " + (d.getMonth() + 1) + "/" + d.getDate() +"/" + d.getFullYear();
+let time = "Time: " + d.getHours() + ":" + (d.getMinutes());
+console.log("Today Date is : "+today) ;

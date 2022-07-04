eb2552b400fc7b62f327ecee256115bd3968cc68
chinki kumari singh <singhchinkikumari.com>
2022-07-04 23:49:24 +0530

    [Chinki] calculate monthly wages

--- EmployeWageJs.js
+++ EmployeWageJs.js
@@ -33,10 +33,13 @@
         }
     }
        
-    let empHrs = 0;
+    const NUM_OF_WORKING_DAYS = 20;
+    let totalEmpHrs = 0;
+    for (let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
         let empCheck = Math.floor(Math.random() * 10) % 3;
-        empHrs = getWorkingHours(empCheck);
-        let empWage = empHrs * WAGE_PER_HOUR;
-        console.log("Hour: "+empHrs)
+        totalEmpHrs += getWorkingHours(empCheck);
+    }
+    let empWage = totalEmpHrs * WAGE_PER_HOUR;
+        console.log("Hour: "+totalEmpHrs)
         console.log("Uc3 - Emp Wage: " + empWage);
 
